cf2eae26be2d398cf4be5401841f86065f1594e5
HieA1lpha <engkauanrodrigues@gmail.com>
2024-10-12 16:48:20 -0300

    2024-10-12

--- main.c
+++ main.c
@@ -20,26 +20,5 @@ int main()
 
 void teste()
 {
-    Pilha *top = NULL;        // Ponteiro que aponta para o topo da pilha, inicialmente nulo
-    char inteiro[] = "%d\n"; // array de caracteres para formar a string
     
-
-    if (top == NULL || (top != NULL && top->data < MAX))
-    {
-        int value = 1;
-
-        // Cria um novo nó e aloca memória para ele
-        Pilha *newPilha = (Pilha *)malloc(sizeof(Pilha));
-        newPilha->data = value; // Armazena o valor no novo nó
-        newPilha->next = top;   // Faz o novo nó apontar para o antigo topo
-        top = newPilha;         // Atualiza o topo para o novo nó
-
-        // Mensagem de sucesso
-        printf(inteiro, value);
-    }
-    else
-    {
-        // Mensagem se a pilha estiver cheia
-        printf("Pilha cheia!\n");
-    }
 }
